b13ae1ec5e9354a94ed182af1e8e4ced63aecf7c
krunalchavhan26 <krunalchavhan5851@gmail.com>
2025-11-23 16:08:03 +0530

    variables

---
+++ 02_variables/variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "golang"
+
+	// infer
+	// var name = "golang"
+	// var isAdult = true
+	// var age int = 30
+
+	// Shorthand syntax
+	// name := "golang"
+
+	// var name string
+	// name = "golang"
+
+	var price float32 = 50.5
+
+	fmt.Println(price)
+
+}
