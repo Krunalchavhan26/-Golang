ecb7472bc774166d743ef4c06ac1a2889e1a9691
krunalchavhan26 <krunalchavhan5851@gmail.com>
2025-11-23 16:56:57 +0530

    if else statements

---
+++ 05_if_else/ifelse.go
@@ -0,0 +1,38 @@
+package main
+
+func main() {
+	// age := 16
+
+	// if age >= 18 {
+	// 	fmt.Println("Person is an adult")
+	// } else {
+	// 	fmt.Println("Person is not an adult")
+	// }
+
+	// age := 16
+
+	// if age >= 18 {
+	// 	fmt.Println("Person is an adult")
+	// } else if age >= 12 {
+	// 	fmt.Println("Person is teenager")
+	// } else {
+	// 	fmt.Println("Person is a kid")
+	// }
+
+	// var role = "admin"
+	// var hasPermissions = false
+
+	// if role == "admin" || hasPermissions {
+	// 	fmt.Println("Yes")
+	// }
+
+	// we can declare a variable inside if construct.
+	// if age := 20; age >= 18 {
+	// 	fmt.Println("Person is an adult", age)
+	// } else if age >= 12 {
+	// 	fmt.Println("Person is teenager", age)
+	// }
+
+	// go does not have ternary, you will have to use normal if else
+
+}
