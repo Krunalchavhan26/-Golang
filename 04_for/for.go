d4b5937f0805b7391fa5eafa89ddc9e1170370d4
krunalchavhan26 <krunalchavhan5851@gmail.com>
2025-11-23 16:36:52 +0530

    for

---
+++ 04_for/for.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// for -> Only construct in go for looping
+func main() {
+	// while loop
+	// i := 1
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	// infinite loop
+	// for {
+	// 	println(1)
+	// }
+
+	// classic for loop
+
+	// for i := 0; i < 3; i++ {
+	// 	if i == 2 {
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	// 1.22
+
+	for i := range 3 {
+		fmt.Println(i)
+	}
+}
